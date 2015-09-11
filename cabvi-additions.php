7f71eaa972da13e8f968d66362a8ad3bc6cc07cc
Damon Maldonado <damon@mpwmarketing.com>
2015-09-11 13:59:33 -0400

    added linked in, new icons

--- cabvi-additions.php
+++ cabvi-additions.php
@@ -16,7 +16,7 @@
   
     function add_social() {
     echo '<div class="social">Follow us on<br />
-		<div class="social-icons"><a href="https://www.facebook.com/cabvi1929" target="_blank"><img src="/wp-content/uploads/2015/01/FB-f-Logo__blue_50.png" /></a>
-		</div><div class="social-icons"><a href="https://www.linkedin.com/company/1334720" target="_blank"><img src="http://www.cabvi.org/wp-content/uploads/2015/08/In-2C-54px-R.png" /></a>
+		<div class="social-icons"><a href="https://www.facebook.com/cabvi1929" target="_blank"><img src="http://www.cabvi.org/wp-content/uploads/2015/08/facebook.png" /></a>
+		</div><div class="social-icons"><a href="https://www.linkedin.com/company/1334720" target="_blank"><img src="http://www.cabvi.org/wp-content/uploads/2015/08/linkedin.png" /></a>
 		</div></div>';
 }
